97e154b49c2125af2855da600156b4e2875e31ad
Sergey Goncharov <goncharov96snz@gmail.com>
2024-09-11 20:23:24 +0500

    Add internal implementation

--- src/main/java/ru/alishev/springcourse/firstSecurity/config/SecurityConfig.java
+++ src/main/java/ru/alishev/springcourse/firstSecurity/config/SecurityConfig.java
@@ -1,24 +1,33 @@
 package ru.alishev.springcourse.firstSecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.alishev.springcourse.firstSecurity.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.alishev.springcourse.firstSecurity.services.PersonDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     // Настраивает аунтификацию
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
     }
 
+    // Как шифруется пароль
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        // Пароль никак не шифруется
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
