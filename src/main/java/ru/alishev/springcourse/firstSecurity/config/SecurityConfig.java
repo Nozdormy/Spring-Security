c4e13ccd5379ee18a1700a08af3d8d339ea6d14d
Sergey Goncharov <goncharov96snz@gmail.com>
2024-09-12 18:51:00 +0500

    Added my own login page

--- src/main/java/ru/alishev/springcourse/firstSecurity/config/SecurityConfig.java
+++ src/main/java/ru/alishev/springcourse/firstSecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.alishev.springcourse.firstSecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,7 +20,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
-    // Настраивает аунтификацию
+    //Конфигурируем авторизацию
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Используем свое представление ввода логина пароля
+        http.csrf().disable()     // Отключение защиты от межсайтовой подделки запросов
+                .authorizeRequests()                      // все запросы проходят через авторизацию
+                .antMatchers("/auth/login", "/error").permitAll()//Пускаем всех без аутетификации
+                .anyRequest().authenticated()         //Для всем других запросов должен быть аутентифицирован
+                .and()                                //Переход к настройке страницы логина
+                .formLogin().loginPage("/auth/login") //Адрес страницы с логином
+                .loginProcessingUrl("/process_login") //Адрес куда пойдут данные с формы
+                .defaultSuccessUrl("/hello", true) //Куда переправить после удачной аутентификации
+                .failureUrl("/auth/login?error");  //Куда после неудачной
+    }
+
+    // Настраиваем аунтификацию
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
